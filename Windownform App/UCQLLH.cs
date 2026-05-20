092e1c4e2ffe7c5520a70340dc75f005f2e911ab
Dinh Dinh Huy <dinhdinhhuyhla@gmail.com>
2026-05-20 23:59:41 +0700

    feat add UserControl + MenuStrip

--- Windownform App/UCQLLH.cs
+++ Windownform App/UCQLLH.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,11 +10,11 @@ using System.Windows.Forms;
 
 namespace Windownform_App
 {
-    internal class Program
+    public partial class UCQLLH : UserControl
     {
-        static void Main(string[] args)
+        public UCQLLH()
         {
-            Application.Run(new form_main());
+            InitializeComponent();
         }
     }
 }
---
+++ Windownform App/form_main.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Windownform_App
+{
+    public partial class form_main : Form
+    {
+        public form_main()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadUserControl(UserControl uc)
+        {
+            panelMain.Controls.Clear();
+
+            uc.Dock = DockStyle.Fill;
+
+            panelMain.Controls.Add(uc);
+        }
+
+        private void quảnLýSinhViênToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            UCQLSV ucqlsv = new UCQLSV();
+            LoadUserControl(ucqlsv);
+        }
+
+        private void quảnLýLớpHọcToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            UCQLLH ucqllh = new UCQLLH();
+            LoadUserControl(ucqllh);
+        }
+    }
+}
