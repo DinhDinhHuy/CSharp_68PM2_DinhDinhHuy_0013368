f3417386b304ab6ea56bee8e4e86d37782e4df5f
Dinh Dinh Huy <dinhdinhhuyhla@gmail.com>
2026-06-15 19:42:49 +0700

    feat<sinhvien>: DataGridView CellClickEvent

--- Windownform App/UCQLSV.cs
+++ Windownform App/UCQLSV.cs
@@ -10,6 +10,7 @@ namespace Windownform_App
         public UCQLSV()
         {
             InitializeComponent();
+
             DataQLSV.AutoGenerateColumns = false;
 
             maSV.DataPropertyName = "ma_sv";
@@ -17,6 +18,8 @@ namespace Windownform_App
             gioitinh.DataPropertyName = "gioitinh";
             ngaysinh.DataPropertyName = "ngay_sinh";
             lop.DataPropertyName = "ma_lop";
+
+            DataQLSV.CellClick += DataQLSV_CellClick;
         }
 
         private void UCQLSV_Load(object sender, EventArgs e)
@@ -34,25 +37,45 @@ namespace Windownform_App
             DataQLSV.DataSource = dssv;
         }
 
+        private void DataQLSV_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0) return;
+
+            DataGridViewRow row = DataQLSV.Rows[e.RowIndex];
+
+            txt_mssv.Text = row.Cells["maSV"].Value?.ToString();
+            txt_hoten.Text = row.Cells["hoten"].Value?.ToString();
+            txt_gioitinh.Text = row.Cells["gioitinh"].Value?.ToString();
+
+            if (row.Cells["ngaysinh"].Value != null)
+            {
+                txt_date.Value = Convert.ToDateTime(row.Cells["ngaysinh"].Value);
+            }
+
+            // txt_lop.Text = row.Cells["lop"].Value?.ToString();
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
             string HoVaTen = txt_hoten.Text;
             string MaSinhVien = txt_mssv.Text;
             DateTime NgaySinh = txt_date.Value;
             string GioiTinh = txt_gioitinh.Text;
-            //string Lop = txt_lop.Text;
+            // string Lop = txt_lop.Text;
 
             sinh_vien sv = new sinh_vien();
+
             sv.ma_sv = MaSinhVien;
             sv.ho_ten = HoVaTen;
             sv.ngay_sinh = NgaySinh;
             sv.gioitinh = GioiTinh;
-            //sv.ma_lop = Lop;
+            // sv.ma_lop = Lop;
 
             DataClasses1DataContext db = new DataClasses1DataContext();
 
             db.sinh_viens.InsertOnSubmit(sv);
             db.SubmitChanges();
+
             loadData();
         }
     }
